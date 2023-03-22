8ddc19e79a51ffba9a48f550c25ab901b8d6b6f1
Smstudo <118610472+Smstudo@users.noreply.github.com>
2023-03-22 08:52:49 -0700

    Delete 1.bat

--- 1.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cls
-title 1
-::--------------------------------------
-:: Remove Firewall Rules
-Reg.exe delete "HKLM\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" /f
-cls
-exit
